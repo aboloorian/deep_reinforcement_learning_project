37d4c356510bf7ca44fda9588198630d40338a01
AntoineTSIO <antoine.touzart@outlook.com>
2024-07-20 17:26:19 +0200

    feat: move MonteCarloES from standard files to notebook

--- RL_project/RL/Parameters.py
+++ RL_project/RL/Parameters.py
@@ -1,5 +1,5 @@
 class Parameters:
-    def __init__(self, size, goal_state, rewards,gamma):
+    def __init__(self, size, goal_state, rewards, gamma):
         self.size = size
         self.goal_state = goal_state
         self.rewards = rewards
--- RL_project/main.py
+++ RL_project/main.py
@@ -14,56 +14,27 @@ from RL.Parameters import Parameters
 # +---+---+---+
 # |S  | 0 | 10 |
 # +---+---+---+
-param = Parameters((3, 3),8,{8: 10,3:-5},1.0)
-# Initialize the Environment
+#
+# Exemple d'utilisation
+param = Parameters((3, 3), 8, {8: 10, 3: -5}, 1.0)
 environment = Environment(param.size, param.goal_state, param.rewards)
 
-# # Initialize the Agent for Policy Iteration
-# agent = PolicyAgent(environment,param)
+# Utilisation de l'agent de politique
+policy_agent = PolicyAgent(environment, param)
+policy_agent.policy_iteration()
 
-# # Perform policy iteration
-# agent.policy_iteration()
-
-# # Print the state values and policy
-# print("State Values:")
-# print(agent.state_values.reshape(param.size))
-# print("\nPolicy:")
-# for row in range(param.size[0]):
-#     for col in range(param.size[1]):
-#         state = row * param.size[1] + col
-#         if state == param.goal_state:
-#             print(" G ", end=" ")
-#         else:
-#             print(agent.policy[state], end=" ")
-#     print()
-
-#  # Find and print the best path from a starting state to the goal state
-# start_state = 0
-# best_path = agent.find_best_path_for_goal(start_state)
-# print("\nBest Path from state 0 to goal:")
-# print(best_path)
-
-# Initialize the Agent for Monte Carlo Prediction
-agent = MonteCarloAgent(environment, gamma=0.9)  # Create an instance of MonteCarloAgent
-
-# Perform policy iteration
-agent.policy_iteration()
-
-# Print the state values and policy
-print("State Values:")
-print(agent.state_values.reshape(param.size))
-print("\nPolicy:")
+print("Valeurs des états (Policy Agent):")
+print(policy_agent.state_values.reshape(param.size))
+print("\nPolitique (Policy Agent):")
 for row in range(param.size[0]):
     for col in range(param.size[1]):
         state = row * param.size[1] + col
         if state == param.goal_state:
             print(" G ", end=" ")
         else:
-            print(agent.policy[state], end=" ")
+            print(policy_agent.policy[state], end=" ")
     print()
 
-# Find and print the best path from a starting state to the goal state
-start_state = 0
-best_path = agent.find_best_path_for_goal(start_state)
-print("\nBest Path from state 0 to goal:")
+best_path = policy_agent.find_best_path_for_goal(start_state)
+print("\nMeilleur chemin (Policy Agent) de l'état 0 à l'objectif:")
 print(best_path)
