8d9293370e47b23b377e2fc52c71dd200d7f8416
AntoineTSIO <antoine.touzart@outlook.com>
2024-07-21 20:28:33 +0200

    clean repo

--- RL_project/RL/Parameters.py
+++
@@ -1,6 +0,0 @@
-class Parameters:
-    def __init__(self, size, goal_state, rewards,gamma):
-        self.size = size
-        self.goal_state = goal_state
-        self.rewards = rewards
-        self.gamma = gamma
